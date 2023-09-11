c5e66ae8beb371bb1d0239bdee1f6e42dea6be33
thomas murat <thomas.murat974@gmail.com>
2023-09-11 17:07:01 +0200

    add association

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,6 +1,7 @@
 class Therapist < ApplicationRecord
   has_one_attached :photo
   has_one :user, dependent: :destroy
+  has_and_belongs_to_many :services
 
   validates :location_name, presence: true
   validates :location_address, presence: true
