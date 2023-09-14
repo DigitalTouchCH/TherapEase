2bd9585eeef8ebc402c4c10359193dcc6e93bf51
Li Zhu <lizhu129@gmail.com>
2023-09-14 23:34:40 +0200

    new and edit absences

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -1,2 +1,45 @@
 class AbsencesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :validate_therapist
+  before_action :set_absence, only: [:edit, :update]
+
+  def index
+    @absences = Absence.all
+  end
+
+  def new
+    @absence = Absence.new
+  end
+
+  def create
+    @absence = Absence.new(absence_params)
+    @absence.therapist = Therapist.where(user: current_user)[0]
+    if @absence.save
+      redirect_to absences_path # TODO: maybe another route?
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @absence.update(absence_params)
+    redirect_to absences_path # TODO: maybe another route?
+  end
+
+  private
+
+  def absence_params
+    params.require(:absence).permit(:start_date_time, :end_date_time, :reason)
+  end
+
+  def set_absence
+    @absence = Absence.find(params[:id])
+  end
+
+  def validate_therapist
+    redirect_to root_path unless Therapist.where(user: current_user).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+
+  resources :absences, only: [:index, :new, :create, :edit, :update]
 end
