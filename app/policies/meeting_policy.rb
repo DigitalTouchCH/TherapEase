9fac592cb761f8c854072b9d27fee91735631229
thomas murat <thomas.murat974@gmail.com>
2023-09-17 13:56:55 +0200

    meeting controller et policies

---
+++ app/policies/meeting_policy.rb
@@ -0,0 +1,78 @@
+class MeetingPolicy < ApplicationPolicy
+
+  def index?
+    # Only therapists and patients associated with the meeting can see the list of meetings
+    user_meetings.any?
+  end
+
+  def show?
+    # The associated therapist or the patient can view a specific meeting
+    user_is_therapist? || user_is_patient?
+  end
+
+  def create?
+    # Only patients can create a meeting
+    user_is_patient?
+  end
+
+  def update?
+    # Only the associated therapist can update the meeting
+    user_is_therapist_for_meeting?
+  end
+
+  def destroy?
+    # Only the associated therapist can delete the meeting
+    user_is_therapist_for_meeting?
+  end
+
+  private
+
+  def user_is_therapist?
+    # Check if the current user is a therapist
+    user.therapist.present?
+  end
+
+  def user_is_patient?
+    # Check if the current user is a patient
+    user.patient.present?
+  end
+
+  def user_is_therapist_for_meeting?
+    # Check if the therapist associated with the package linked to the meeting is the current user
+    record.package.therapist == user.therapist
+  end
+
+  def user_meetings
+    # Return a list of meetings associated with the current user, either as a therapist or as a patient
+    if user_is_therapist?
+      Meeting.joins(:package).where(packages: { therapist: user.therapist })
+    elsif user_is_patient?
+      Meeting.joins(:package).where(packages: { patient: user.patient })
+    else
+      Meeting.none
+    end
+  end
+
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.therapist.present?
+        # If the user is a therapist, retrieve all meetings associated with them
+        scope.joins(:package).where(packages: { therapist: user.therapist })
+      elsif user.patient.present?
+        # If the user is a patient, retrieve all meetings associated with them
+        scope.joins(:package).where(packages: { patient: user.patient })
+      else
+        # If the user is neither, return no meetings
+        scope.none
+      end
+    end
+  end
+end
