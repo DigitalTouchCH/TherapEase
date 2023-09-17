2724ea702aa0676c48a8e809adc961a04581cc79
thomas murat <thomas.murat974@gmail.com>
2023-09-17 22:48:28 +0200

    seedbooking

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
   belongs_to :package
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_and_belongs_to_many :media, join_table: 'media_meetings'
 
   validates :max_attendees, numericality: { greater_than_or_equal_to: 1}
