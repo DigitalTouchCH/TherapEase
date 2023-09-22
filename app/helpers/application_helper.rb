51282968eddf41264f96b2b1b8970deb42b6bc46
Leon Linton <leonmlinton@gmail.com>
2023-09-22 23:53:51 +0100

    prices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def youtube_video(url)
     render :partial => 'shared/video', :locals => { :url => url }
   end
+
+  def format_price_per_unit(price)
+    if price.to_i == price.to_f
+      price.to_i.to_s
+    else
+      number_to_currency(price)
+    end
+  end
 end
